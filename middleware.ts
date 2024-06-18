a25cf3d9c1538a06ef2b519f0ac4fea2a67935d2
vicki-robertson <135024584+vicki-robertson@users.noreply.github.com>
2024-06-18 22:12:54 +0200

    webhook added and createUser connected to db

--- middleware.ts
+++ middleware.ts
@@ -6,6 +6,9 @@ export default clerkMiddleware((auth, req) => {
   if (isProtectedRoute(req)) auth().protect()
 })
 
+//Webhook events will never be signed in. They're coming from a source outside of your app, in signed-out state.
+//As in signed-out state, the route should be public! e.g. sign-in, sign-up page
+
 export const config = {
   matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)']
 }
